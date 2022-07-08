01eb9917ac18bb750fb9f92e196b91708bfcc3a1
Carlos Augusto <carlosaugustomfe@gmail.com>
2022-07-08 15:53:43 -0400

    Criei tabela001

---
+++ desafios/primeiro-contato-com-js/script.js
@@ -0,0 +1,16 @@
+class DankiCode{
+
+    constructor() {
+        this.nome = 'Guto'
+        console.log(this.nome);
+
+    }
+
+    printNome () {
+        console.log(this.nome);
+    }
+
+}
+
+var dankiCode = new DankiCode('Guto');
+
